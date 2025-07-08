95465b079c68a31e2ee7376c68d4626202f8fdb4
Nidishkumar S <nidishk06@gmail.com>
2025-07-08 17:40:03 +0530

    Delete todo directory

--- todo/forms.py
+++
@@ -1,9 +0,0 @@
-# todo/forms.py
-
-from django import forms
-from .models import Task
-
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = ['title', 'completed']
